ec2c940d5a503d040cd879b64d22e14de379b7c1
Tiago Almeida da Rosa <tiago.almeida@poatek.com>
2023-02-06 13:19:51 -0300

    Unnecessary reference to appointmentsApiUrl

--- src/services/appointment/index.tsx
+++ src/services/appointment/index.tsx
@@ -1,34 +1,27 @@
-import { appointmentsApiUrl } from "@/config";
 import { httpClient } from "@/httpClient";
 import { Appointment } from "./interfaces";
 
 export class AppointmentService {
   static getAppointments(courtId: number, date: string) {
     return httpClient.get<Appointment[]>(
-      `${appointmentsApiUrl}/courts/${courtId}/appointments?date=${date}`
+      `/courts/${courtId}/appointments?date=${date}`
     );
   }
 
   static addAppointment(courtId: number, appointment: Appointment) {
-    return httpClient.post(
-      `${appointmentsApiUrl}/courts/${courtId}/appointments`,
-      appointment
-    );
+    return httpClient.post(`/courts/${courtId}/appointments`, appointment);
   }
 
   static updateAppointment(courtId: number, appointment: Appointment) {
-    return httpClient.put(
-      `${appointmentsApiUrl}/courts/${courtId}/appointments/${appointment.id}`,
-      {
-        customerName: appointment.customerName,
-        customerPhoneNumber: appointment.customerPhoneNumber,
-      }
-    );
+    return httpClient.put(`/courts/${courtId}/appointments/${appointment.id}`, {
+      customerName: appointment.customerName,
+      customerPhoneNumber: appointment.customerPhoneNumber,
+    });
   }
 
   static deleteAppointment(courtId: number, appointmentId: number) {
     return httpClient.delete(
-      `${appointmentsApiUrl}/courts/${courtId}/appointments/${appointmentId}`
+      `/courts/${courtId}/appointments/${appointmentId}`
     );
   }
 }
