b6c9bbfbe80cba7357444d59a67d4b812a6d0389
Tiago Almeida da Rosa <tiago.almeida@poatek.com>
2023-04-03 18:24:10 -0300

    Correct loading

--- src/components/Background/freeCourts.tsx
+++ src/components/Background/freeCourts.tsx
@@ -40,62 +40,58 @@ const FreeCourts = ({ imagePoints, onSelect }: Props) => {
   }, []);
 
   return (
-    <>
-      {isLoading && <Spin />}
-      {!isLoading && (
-        <>
-          <div className={styles.absolute}>
-            <div className={styles.title}>
-              <Text>{"Horários disponíveis para marcação hoje:"}</Text>
-            </div>
-            <div className={styles.body}>
-              {imagePoints.map((imagePoint: ImagePoint) => {
-                return (
-                  <div key={`court_${imagePoint.id}`}>
-                    <Text>{imagePoint.title}</Text>
-                    <div className={styles.buttons}>
-                      <>
-                        {getHours(new Date(), 18, 22).map(
-                          (hour: number, index: number) => {
-                            var courtId = parseInt(imagePoint.id);
-                            var appointments = courtsAppointments?.find(
-                              (ca: CourtAppointment) => ca.id === courtId
-                            )?.appointments;
-
-                            var formattedHour = `${zeroPad(hour)}:00:00`;
-                            var appointment = appointments?.find(
-                              (a: Appointment) => a.time === formattedHour
-                            );
-                            if (appointment) {
-                              return;
-                            }
-                            return (
-                              <Button
-                                key={`hour_${index}`}
-                                type="primary"
-                                onClick={() =>
-                                  onSelect(
-                                    courtId,
-                                    ModalSteps.step2,
-                                    formattedHour
-                                  )
-                                }
-                              >
-                                {formattedHour.substring(0, 5)}
-                              </Button>
-                            );
-                          }
-                        )}
-                      </>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </>
+    <div className={styles.absolute}>
+      <div className={styles.title}>
+        <Text>{"Horários disponíveis para marcação hoje:"}</Text>
+      </div>
+      {isLoading && (
+        <div className={styles.center}>
+          <Spin />
+        </div>
       )}
-    </>
+      {!isLoading && (
+        <div className={styles.body}>
+          {imagePoints.map((imagePoint: ImagePoint) => {
+            return (
+              <div key={`court_${imagePoint.id}`}>
+                <Text>{imagePoint.title}</Text>
+                <div className={styles.buttons}>
+                  <>
+                    {getHours(new Date(), 18, 22).map(
+                      (hour: number, index: number) => {
+                        var courtId = parseInt(imagePoint.id);
+                        var appointments = courtsAppointments?.find(
+                          (ca: CourtAppointment) => ca.id === courtId
+                        )?.appointments;
+
+                        var formattedHour = `${zeroPad(hour)}:00:00`;
+                        var appointment = appointments?.find(
+                          (a: Appointment) => a.time === formattedHour
+                        );
+                        if (appointment) {
+                          return;
+                        }
+                        return (
+                          <Button
+                            key={`hour_${index}`}
+                            type="primary"
+                            onClick={() =>
+                              onSelect(courtId, ModalSteps.step2, formattedHour)
+                            }
+                          >
+                            {formattedHour.substring(0, 5)}
+                          </Button>
+                        );
+                      }
+                    )}
+                  </>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
   );
 };
 
